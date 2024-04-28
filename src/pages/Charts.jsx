5ea66b4804f15174234da1f3d10ec3d29f59b7c3
Akanksha Bankhele <akankshabankhele@Akankshas-MacBook-Air.local>
2024-04-28 16:38:43 -0400

    data viz4 added

--- src/pages/Charts.jsx
+++ src/pages/Charts.jsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import DataViz1 from "../Components/DataViz1";
 import DataViz2 from "../Components/DataViz2";
 import DataViz3 from "../Components/DataViz3";
+import DataViz4 from "../Components/DataViz4";
 
 const Charts = () => {
     return (
@@ -11,6 +12,7 @@ const Charts = () => {
 
                 <DataViz2 />
                 <DataViz3 />
+               <DataViz4/>
             </div>
 
         </>
